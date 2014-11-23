6968db96b0b13c8cf9dff2755e1a3dfea0f5de39
Anas Nakawa <anas.nakawa@gmail.com>
2014-11-23 23:05:32 +0400

    components / serverless / ejs
    - adding bower components
    - working without localhost / server
    - converting some pages into reusable els templates

--- gruntfile.js
+++ gruntfile.js
@@ -6,35 +6,29 @@ var _ = require( 'underscore' );
 var loadGruntTasks = require( 'load-grunt-tasks' );
 var pkg = require( './package.json' );
 
-function ejsPageList() {
-
-  var result = {};
-
-  pkg.ejs.pages.forEach(function( page ) {
-    result[ page ] = {
-      src: [ 'ejs/pages/{page}.ejs'.replace( /{page}/, page ) ],
-      dest: 'dist/pages/{page}.html'.replace( /{page}/, page )
-    }
-  });
-
-  return result;
-}
-
 module.exports = function( grunt ) {
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    less: {
-      admin: {
-        files: {
-          './css/admin.css': './less/admin.less'
+        less: {
+            admin: {
+                files: {
+                    './css/admin.css': './less/admin.less'
+                }
+            }
+        },
+
+        ejs: {
+            all: {
+                src: [ './ejs/**/*.ejs', '!./ejs/base/**/*.ejs', '!./ejs/partials/**/*.ejs' ]
+                , dest: './'
+                , expand: true
+                , flatten: true 
+                , ext: '.html'
+            }
         }
-      }
-    },
 
-    ejs: _.extend( ejsPageList(), {})
+    })
 
-  })
-
-  loadGruntTasks( grunt );
+    require( 'load-grunt-tasks' )( grunt );
 }
